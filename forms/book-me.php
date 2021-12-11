1ba57f37135502f0c174f1e662199b31e9ad5e05
aambrioso01 <40615096+aambrioso01@users.noreply.github.com>
2021-12-11 18:13:34 -0500

    integrated with Bootstrap

---
+++ forms/book-me.php
@@ -0,0 +1,19 @@
+<?php
+
+    if(isset($_POST['sumbit']))
+    {
+        $name = $_POST['name'];
+        $mailFrom = $_POST['email'];
+        $phone = $_POST['phone'];
+        $mailFrom = $_POST['date'];
+        $name = $_POST['type'];
+
+        $mailTo = "sparkcles@mail.com";
+        $headers = "From: ".$nameFrom;
+        $txt = "New contact from " .$name.".\n\n".$message;
+
+        mail(mailTo, $subject, $headers, $txt);
+        header("Location: index.php?mailsend");
+    }
+
+?>
