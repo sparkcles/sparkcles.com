f047ae688e0a3c391099ffedf700ebb0d03bc09d
aambrioso01 <40615096+aambrioso01@users.noreply.github.com>
2021-12-14 08:42:15 -0500

    contact-me form
    debugging

---
+++ forms/contact-me.php
@@ -0,0 +1,16 @@
+<?php
+
+    if(isset($_POST['submit']))
+    {
+        $name = $_POST['name'];
+        $mailFrom = $_POST['email'];
+        $subject = $_POST['subject'];
+        $message = $_POST['message'];
+
+        $mailTo = "sparkcles@mail.com";
+        $headers = "From: ".$mailFrom;
+        $txt = "New contact from ".$name.".\n\n".$message;
+
+        mail(mailTo, $subject, $txt, $headers);
+        header("Location: index.php?mailsend");
+    }
