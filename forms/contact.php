7970877663d66c06ac371a88e771ed82d77bf58f
aambrioso01 <40615096+aambrioso01@users.noreply.github.com>
2021-12-21 10:22:40 -0500

    google verification

--- forms/contact.php
+++ forms/contact.php
@@ -6,6 +6,9 @@
   * For more info and help: https://bootstrapmade.com/php-email-form/
   */
 
+  
+
+
   // Replace contact@example.com with your real receiving email address
   $receiving_email_address = 'contact@example.com';
 
